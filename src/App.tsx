99fffe495086fc7230ac0dfb4b05ec29a9cceb60
vipzero <hiroflighter@gmail.com>
2019-07-04 18:17:09 +0900

    fix: api configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useLocalStorage from 'react-use/lib/useLocalStorage'
-import { generatePemo } from './pemo'
+import useSessionStorage from 'react-use/lib/useSessionStorage'
+import { generatePemo, generatePemora, generatePemon } from './pemo'
 import { Button } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
@@ -10,7 +11,15 @@ function App() {
 		'text',
 		'ペモペモ語を作ります。'
 	)
-	const [pemoText, setRes] = useLocalStorage<any>('---')
+	const [pemonText, setPemonText] = useSessionStorage<string>(
+		'pemonText',
+		'---'
+	)
+	const [pemoraText, setPemoraText] = useSessionStorage<string>(
+		'pemoraText',
+		'---'
+	)
+	const [pemoText, setPemo] = useSessionStorage<string>('pemoText', '---')
 	return (
 		<div className="App">
 			<Typography variant="h2">ぺもじぇねれーた</Typography>
@@ -25,23 +34,22 @@ function App() {
 			/>
 			<Button
 				onClick={async () => {
-					setRes(await generatePemo(text))
+					setPemoraText(generatePemora(text))
+					setPemonText(await generatePemon(text))
+					setPemo(await generatePemo(text))
 				}}
 			>
 				翻訳
 			</Button>
-			<Typography variant="h6">ぺも語</Typography>
-			<TextField
-				fullWidth
-				InputProps={{
-					readOnly: true,
-				}}
-				variant="outlined"
-				value={pemoText}
-				onChange={e => {
-					setText(e.target.value)
-				}}
-			/>
+			<Typography variant="h5">ぺもら語</Typography>
+			<Typography>ランダム・一意・非可逆</Typography>
+			<TextField fullWidth variant="outlined" value={pemoraText} />
+			<Typography variant="h5">ぺもん語</Typography>
+			<Typography>長い・一意・可逆</Typography>
+			<TextField fullWidth variant="outlined" value={pemonText} />
+			<Typography variant="h5">ぺも語</Typography>
+			<Typography>一意・可逆</Typography>
+			<TextField fullWidth variant="outlined" value={pemoText} />
 		</div>
 	)
 }
--- src/pemo.ts
+++ src/pemo.ts
@@ -1,12 +1,24 @@
 import _ from 'lodash'
 import axios from 'axios'
 
-axios.defaults.headers = {
-	Authorization: `Bearer ${process.env.REACT_APP_CNL_TOKEN}`,
-	'Content-Type': `application/json; charset=utf-8`,
-}
+const langClient = axios.create({
+	baseURL: `https://language.googleapis.com/v1/documents:analyzeSyntax?key=${
+		process.env.REACT_APP_CNL_TOKEN
+	}`,
+	headers: {
+		'Content-Type': `application/json; charset=utf-8`,
+	},
+})
+const postAnalyze = (content: string) =>
+	langClient.post('', {
+		encodingType: 'UTF8',
+		document: {
+			content,
+			type: 'PLAIN_TEXT',
+		},
+	})
 
-const Base64 = {
+const base64 = {
 	encode: (str: string) => btoa(unescape(encodeURIComponent(str))),
 	decode: (str: string) => decodeURIComponent(escape(atob(str))),
 }
@@ -36,24 +48,21 @@ const joshi: Record<string, string> = {
 	など: 'っろ',
 }
 
-// 固有名詞は「」に囲む
-export async function generatePemo(text: string) {
-	const document = {
-		content: text,
-		type: 'PLAIN_TEXT',
-	}
+export function generatePemora(text: string): string {
+	return 'pemorad'
+}
 
+export async function generatePemon(text: string) {
+	return '開発中'
+}
+
+export async function generatePemo(text: string) {
 	// Detects the sentiment of the text
-	const res = await axios.post(
-		'https://language.googleapis.com/v1/documents:analyzeSyntax',
-		{
-			encodingType: 'UTF8',
-			document,
-		}
-	)
+
+	const res = await postAnalyze(text)
 	console.dir(res)
 	// console.log(`Text: ${text}`)
 	// console.log(`Sentiment score: ${sentiment.score}`)
 	// console.log(`Sentiment magnitude: ${sentiment.magnitude}`)
-	return ''
+	return '開発中'
 }
